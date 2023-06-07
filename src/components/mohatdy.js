e68e2b90d3e9a76463249c2fcbd48fc01576e2e4
mahmoud-mohtady <mahmoud.mohtady.20@gmail.com>
2023-06-07 15:26:12 +0300

    adding a new header

---
+++ src/components/mohatdy.js
@@ -0,0 +1,38 @@
+
+export default function Header() {
+
+  return <>
+
+    <div className="container-fluid row main">
+
+      <div className="col-sm- text-center mt-5">
+
+        <h1>MOHTADY</h1>
+
+      </div>
+
+      <div className="col-sm- text-center mt-2">
+
+        <h2>Web Developer</h2>
+
+      </div>
+
+      <div className="col-sm- text-center mt-4">
+
+        <button>
+
+          <a href="#"> Contact Me</a>
+
+        </button>
+
+      </div>
+
+
+    </div>
+
+
+
+  </>
+
+
+}
